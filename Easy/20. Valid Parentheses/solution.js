ded75d34dd02ab0b71602d845a985f6243aa9e92
Bradley <104906904+bradley-03@users.noreply.github.com>
2023-06-07 21:42:45 +0100

    Valid Parentheses js solution

---
+++ Easy/20. Valid Parentheses/solution.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+const tester = "()"
+
+var isValid = function (s) {
+    let stack = []
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '(') {
+            stack.push(')')
+        } else if (s[i] == '{') {
+            stack.push('}')
+        } else if (s[i] == '[') {
+            stack.push(']')
+        } else if (stack.pop() !== s[i]) {
+            return false
+        }
+    }
+    return !stack.length
+};
+
+console.log(isValid(tester))
