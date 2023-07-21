e762c47a9c30d617863ef6554f8ad2968e565144
bradley-03 <104906904+bradley-03@users.noreply.github.com>
2023-07-21 15:15:38 +0100

    Rewrote to not use built in JS methods

--- Easy/28. Find the Index of the First Occurrence in a String/solution.js
+++ Easy/28. Find the Index of the First Occurrence in a String/solution.js
@@ -1,5 +1,19 @@
 // https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle)
+    let needleFound = true
+    for (let i = 0; i < haystack.length; i++) {
+        needleFound = true
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) { // check if needle is found
+                needleFound = false   
+            }
+        }
+        if (needleFound == true) { // return index if found
+            return i 
+        }
+    }
+    return -1 // return -1 as no index was found
 };
+
+console.log(strStr('opkdwkdokpwbutsad', 'sad'))
